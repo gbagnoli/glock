9a8542b244386e503b067e32490202504fe7c77e
Giacomo Bagnoli <gbagnoli@gmail.com>
2016-08-01 11:01:07 +0100

    Use github.com/stvp/tempredis to spin up the redis instance for tests

--- driver_redis_test.go
+++ driver_redis_test.go
@@ -4,18 +4,37 @@ package glock
 
 import (
 	"flag"
+	"os"
 	"testing"
 	"time"
+
+	"github.com/stvp/tempredis"
 )
 
-var network = flag.String("network", "tcp", "Redis network")
-var address = flag.String("address", "localhost:6379", "Redis addr")
+const port = "12345"
+
 var namespace = flag.String("namspace", "glock:tests", "Redis keys namespace")
+var server *tempredis.Server
+
+func TestMain(m *testing.M) {
+	var err error
+	config := map[string]string{
+		"port": port,
+		"bind": "127.0.0.1",
+	}
+	server, err = tempredis.Start(config)
+	if err != nil {
+		panic(err)
+	}
+	result := m.Run()
+	server.Term()
+	os.Exit(result)
+}
 
 func redisClient(t *testing.T) Client {
 	opts := RedisOptions{
-		Network:   *network,
-		Address:   *address,
+		Network:   "tcp",
+		Address:   "127.0.0.1" + ":" + port,
 		Namespace: *namespace,
 	}
 	c1, err := NewRedisClient(opts)
